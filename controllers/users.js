e5af22fbfe2d1f6afe09cceaa4a005068c13549d
LucienneHuang <q22641949@gmail.com>
2023-07-28 16:33:44 +0800

    寫登入、登出、舊換新、取得使用者資料

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,8 @@
 import users from '../models/users.js'
 import { StatusCodes } from 'http-status-codes'
 import { getMessageFromValidationError } from '../utils/error.js'
+import jwt from 'jsonwebtoken'
+
 // 註冊
 export const register = async (req, res) => {
   try {
@@ -27,5 +29,92 @@ export const register = async (req, res) => {
         message: '發生錯誤'
       })
     }
+  }
+}
+
+// 登入
+export const login = async (req, res) => {
+  try {
+    const token = jwt.sign({ _id: req.user._id }, process.env.JWT_SECRET, { expiresIn: '7 days' })
+    req.user.tokens.push(token)
+    await req.user.save()
+    res.status(StatusCodes.OK).json({
+      success: true,
+      message: '',
+      result: {
+        token,
+        email: req.user.email,
+        role: req.user.role,
+        // total 目前加到多少
+        // current 目前的東西
+        cart: req.user.cart.reduce((total, current) => total + current.quantity, 0)
+      }
+    })
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: '發生錯誤'
+    })
+  }
+}
+
+// 登出
+export const logout = async (req, res) => {
+  try {
+    // 用 filter 把一樣的 token 拿掉
+    // 也可以用 findIndex 再 splice，能拿掉就好了
+    req.user.tokens = req.user.tokens.filter(token => token !== req.token)
+    await req.user.save()
+    res.status(StatusCodes.OK).json({
+      success: true,
+      message: ''
+    })
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: '發生錯誤'
+    })
+  }
+}
+
+// 舊換新
+export const extend = async (req, res) => {
+  try {
+    const idx = req.user.token.findIndex(token => token === req.token)
+    const token = jwt.sign({ _id: req.user._id }, process.env.JWT_SECRET, { expiresIn: '7 days' })
+    req.user.token[idx] = token
+    await req.user.save()
+    res.status(StatusCodes.OK).json({
+      success: true,
+      message: '',
+      result: token
+    })
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: '發生錯誤'
+    })
+  }
+}
+
+// 取得使用者資料
+// 會在有登入狀態 + 進網頁時會用到
+// 不需要 async，因為使用者資料已經在 req.user 裡面了
+export const getProfile = (req, res) => {
+  try {
+    res.status(StatusCodes.OK).json({
+      success: true,
+      message: '',
+      result: {
+        email: req.user.email,
+        role: req.user.role,
+        cart: req.user.cart.reduce((total, current) => total + current.quantity, 0)
+      }
+    })
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: '發生錯誤'
+    })
   }
 }

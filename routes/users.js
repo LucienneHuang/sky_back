1dde06f1f2e873df3b3d6956542459b201f7c066
LucienneHuang <q22641949@gmail.com>
2023-07-28 16:33:04 +0800

    user route設定

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,14 @@
 import { Router } from 'express'
 import contentType from '../middlewares/contentType.js'
-import { register } from '../controllers/users.js'
+import { register, login, logout, extend, getProfile } from '../controllers/users.js'
+import * as auth from '../middlewares/auth.js'
+
 const router = Router()
 
 router.post('/', contentType('application/json'), register)
+router.post('/login', contentType('application/json'), auth.login, login)
+router.delete('/logout', auth.jwt, logout)
+router.patch('/extend', auth.jwt, extend)
+router.get('/me', auth.jwt, getProfile)
 
 export default router

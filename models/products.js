a771f8ea863f4d90ae677494f4271a97e73af313
LucienneHuang <q22641949@gmail.com>
2023-08-04 10:37:59 +0800

    改一下錯誤的文字

--- models/products.js
+++ models/products.js
@@ -10,18 +10,18 @@ const schema = new Schema({
   // 商品名稱
   name: {
     type: String,
-    required: [true, '缺少名稱']
+    required: [true, '缺少商品名稱']
   },
   // 商品價格
   price: {
     type: Number,
-    required: [true, '缺少價格'],
+    required: [true, '缺少商品價格'],
     min: [0, '價格太低']
   },
   // 商品價格單位
   currency: {
     type: String,
-    required: [true, '缺少單位'],
+    required: [true, '缺少商品幣值單位'],
     enum: {
       values: ['季票', '愛心', '現金', '噗幣', '其他'],
       message: '找不到 {VALUE} 單位'
@@ -30,7 +30,7 @@ const schema = new Schema({
   // 商品圖片
   image: {
     type: String,
-    required: [true, '缺少圖片']
+    required: [true, '缺少商品圖片']
   },
   // 商品圖片(選填)
   images: {
@@ -40,12 +40,12 @@ const schema = new Schema({
   // 商品說明
   description: {
     type: String,
-    required: [true, '缺少說明']
+    required: [true, '缺少商品說明']
   },
   // 商品分類
   category: {
     type: String,
-    required: [true, '缺少分類'],
+    required: [true, '缺少商品分類'],
     enum: {
       values: ['季票', '禮包', '周邊', '其他'],
       message: '找不到 {VALUE} 分類'
@@ -54,7 +54,7 @@ const schema = new Schema({
   // 是否上架
   sell: {
     type: Boolean,
-    required: [true, '缺少上架狀態']
+    required: [true, '缺少商品上架狀態']
   }
 }, { versionKey: false })
 

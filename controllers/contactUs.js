03d1d959aad8820b0e16f3bb586b4cea03f4de73
LucienneHuang <q22641949@gmail.com>
2023-08-07 19:55:35 +0800

    新增editCheckInfo 和更改getMessage

--- controllers/contactUs.js
+++ controllers/contactUs.js
@@ -1,5 +1,6 @@
 import contactUs from '../models/contactUs.js'
 import { StatusCodes } from 'http-status-codes'
+import { getMessageFromValidationError } from '../utils/error.js'
 
 export const sendMessage = async (req, res) => {
   try {
@@ -18,14 +19,11 @@ export const sendMessage = async (req, res) => {
 }
 export const getMessage = async (req, res) => {
   try {
+    const result = await contactUs.find()
     res.status(StatusCodes.OK).json({
       success: true,
       message: '',
-      result: {
-        name: req.contactUs.name,
-        email: req.contactUs.email,
-        message: req.contactUs.message
-      }
+      result
     })
   } catch (error) {
     console.log(error)
@@ -33,5 +31,44 @@ export const getMessage = async (req, res) => {
       success: false,
       message: '發生錯誤'
     })
+  }
+}
+
+export const editCheckInfo = async (req, res) => {
+  try {
+    const result = await contactUs.findByIdAndUpdate(req.params.id, {
+      done: req.body.done
+    }, { new: true, runValidators: true })
+    if (!result) {
+      throw new Error('NOT FOUND')
+    }
+    res.status(StatusCodes.OK).json({
+      success: true,
+      message: '',
+      result
+    })
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      res.status(StatusCodes.BAD_REQUEST).json({
+        success: false,
+        message: getMessageFromValidationError(error)
+      })
+    } else if (error.name === 'CastError') {
+      console.log(error)
+      res.status(StatusCodes.BAD_REQUEST).json({
+        success: false,
+        message: '格式錯誤'
+      })
+    } else if (error.message === 'NOT FOUND') {
+      res.status(StatusCodes.NOT_FOUND).json({
+        success: false,
+        message: '找不到'
+      })
+    } else {
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
+        success: false,
+        message: '發生錯誤'
+      })
+    }
   }
 }

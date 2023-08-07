d7f1074194a2caa19be694889947499c24e56a72
LucienneHuang <q22641949@gmail.com>
2023-08-07 19:56:53 +0800

    加路由

--- routes/contactUs.js
+++ routes/contactUs.js
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import contentType from '../middlewares/contentType.js'
-import { sendMessage, getMessage } from '../controllers/contactUs.js'
+import { sendMessage, getMessage, editCheckInfo } from '../controllers/contactUs.js'
 import * as auth from '../middlewares/auth.js'
-
+import admin from '../middlewares/admin.js'
 const router = Router()
 
 router.post('/', contentType('application/json'), sendMessage)
-router.get('/getMessage', auth.jwt, getMessage)
+router.get('/get', auth.jwt, admin, getMessage)
+router.patch('/:id', auth.jwt, contentType('application/json'), editCheckInfo)
 
 export default router

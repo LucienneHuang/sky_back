87baf1117c2f1b62853209a3aef50e54e8dc5dc3
LucienneHuang <q22641949@gmail.com>
2023-08-08 18:18:58 +0800

    新增 articles 的路由內容

---
+++ routes/articles.js
@@ -0,0 +1,17 @@
+import { Router } from 'express'
+import contentType from '../middlewares/contentType.js'
+import * as auth from '../middlewares/auth.js'
+import upload from '../middlewares/upload.js'
+import admin from '../middlewares/admin.js'
+import { create, getAll, getRealms, getNews, editArticle, deleteArticle } from '../controllers/articles.js'
+
+const router = Router()
+
+router.post('/', auth.jwt, admin, contentType('multipart/form-data'), upload, create)
+router.get('/all', auth.jwt, admin, getAll)
+router.get('/realms', auth.jwt, admin, getRealms)
+router.get('/getNews', auth.jwt, admin, getNews)
+router.patch('/:id', auth.jwt, admin, upload, contentType('multipart/form-data'), editArticle)
+router.delete('/:id', auth.jwt, admin, deleteArticle)
+
+export default router

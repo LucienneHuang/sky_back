d2aff13e8e856c5ca924c67ba30cbef9aae0714a
LucienneHuang <q22641949@gmail.com>
2023-08-17 22:57:24 +0800

    改

--- routes/orders.js
+++ routes/orders.js
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import contentType from '../middlewares/contentType.js'
 import * as auth from '../middlewares/auth.js'
 import admin from '../middlewares/admin.js'
 import { create, get, getAll, getSell, updateOrder } from '../controllers/orders.js'
@@ -10,7 +11,7 @@ router.post('/', auth.jwt, create)
 router.get('/', auth.jwt, get)
 // 賣家取所有買家
 router.get('/sell', auth.jwt, getSell)
-router.patch('/:id', auth.jwt, updateOrder)
+router.patch('/:id', auth.jwt, contentType('application/json'), updateOrder)
 // 管理員取所有
 router.get('/all', auth.jwt, admin, getAll)
 export default router

8d7eac4d7dfe97a927b2edf3f21abc238592e045
LucienneHuang <q22641949@gmail.com>
2023-08-17 16:02:40 +0800

    debug

--- controllers/orders.js
+++ controllers/orders.js
@@ -9,28 +9,38 @@ export const create = async (req, res) => {
     if (req.user.cart.length === 0) {
       throw new Error('EMPTY')
     }
-    const user = users.findById(req.user._id, 'cart').populate('cart.product')
+    const user = await users.findById(req.user._id, 'cart').populate('cart.productCart.product')
     // every 是不是每個都 return true
     // some 是有沒有任何一個東西 return true
     // filter 是 true 的留著，false 踢掉
-
     // 每一個 cart.product.sell 都是 true，canCheckOut才會是 true
-    const canCheckOut = user.cart.every(cart => cart.product.sell)
-    if (canCheckOut) {
+    const canCheckOut = user.cart.every(cart => {
+      return cart.productCart.every(item => item.product.sell)
+    })
+    if (!canCheckOut) {
       throw new Error('SELL')
     }
     // 建立訂單
-    const result = await orders.create({
-      user: req.user._id,
-      cart: req.user.cart
-    })
+    for (const miniCart in user.cart) {
+      const sum = user.cart[miniCart].productCart.reduce((total, current) => total + (current.quantity * current.product.price), 0)
+      await orders.create({
+        user: user._id,
+        seller: user.cart[miniCart].seller,
+        realName: req.body.realName,
+        phoneNumber: req.body.phoneNumber,
+        address: req.body.address,
+        payment: req.body.payment,
+        date: req.body.date,
+        total: sum,
+        cart: user.cart[miniCart].productCart
+      })
+    }
     // 清空購物車
     req.user.cart = []
     await req.user.save()
-    req.status(StatusCodes.OK).json({
+    res.status(StatusCodes.OK).json({
       success: true,
-      message: '',
-      result
+      message: ''
     })
   } catch (error) {
     if (error.message === 'EMPTY') {
@@ -58,13 +68,15 @@ export const create = async (req, res) => {
 }
 export const get = async (req, res) => {
   try {
-    const result = await orders.find({ user: req.user._id }).populate('cart.product')
-    req.status(StatusCodes.OK).json({
+    const result = await orders.find({ user: req.user._id }).populate('cart.product').populate('seller', 'nickname')
+    console.log(result)
+    res.status(StatusCodes.OK).json({
       success: true,
       message: '',
       result
     })
   } catch (error) {
+    console.log(error)
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
       success: false,
       message: '發生錯誤'

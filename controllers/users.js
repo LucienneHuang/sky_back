7ccff4674f5f1deb28a2885e83f38c9e6134a686
LucienneHuang <q22641949@gmail.com>
2023-08-18 16:40:28 +0800

    delete no need area

--- controllers/users.js
+++ controllers/users.js
@@ -259,64 +259,6 @@ export const getCart = async (req, res) => {
     })
   }
 }
-// export const editCart = async (req, res) => {
-//   try {
-//     // 尋找購物車有沒有我們傳進來的商品 id
-//     const idx = req.user.cart.findIndex(cart => cart.product.toString() === req.body.product)
-//     if (idx > -1) {
-//       // 如果購物車內已經有商品
-//       // 先檢查修改後的數量
-//       const quantity = req.user.cart[idx].quantity + parseInt(req.body.quantity)
-//       if (quantity <= 0) {
-//         // 小於 0 ，移除
-//         req.user.cart.splice(idx, 1)
-//       } else {
-//         // 大於 0 ，修改
-//         req.user.cart[idx].quantity = quantity
-//       }
-//     } else {
-//       // 如果購物車內沒有，檢查商品 id 是否存在
-//       const product = await products.findById(req.body.product)
-//       // 如果沒有或沒上架
-//       if (!product || !product.sell) {
-//         // 錯誤
-//         throw new Error('NOT FOUND')
-//       } else {
-//         // 有，放入購物車
-//         req.user.cart.push({
-//           product: product._id,
-//           seller: req.body.seller,
-//           quantity: req.body.quantity
-//         })
-//       }
-//     }
-//     // 保存
-//     await req.user.save()
-//     res.status(StatusCodes.OK).json({
-//       success: true,
-//       message: '',
-//       result: req.user.cart.reduce((total, current) => total + current.quantity, 0)
-
-//     })
-//   } catch (error) {
-//     if (error.name === 'NOT FOUND') {
-//       res.status(StatusCodes.NOT_FOUND).json({
-//         success: false,
-//         message: '找不到'
-//       })
-//     } else if (error.name === 'ValidationError') {
-//       res.status(StatusCodes.BAD_REQUEST).json({
-//         success: false,
-//         message: getMessageFromValidationError(error)
-//       })
-//     } else {
-//       res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({
-//         success: false,
-//         message: '發生錯誤'
-//       })
-//     }
-//   }
-// }
 
 export const editCart = async (req, res) => {
   try {

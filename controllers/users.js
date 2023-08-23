d70be7f93c945c83dbae172eaa83dfb52ea03e13
LucienneHuang <q22641949@gmail.com>
2023-08-24 03:02:24 +0800

    拿掉 console.log

--- controllers/users.js
+++ controllers/users.js
@@ -112,6 +112,7 @@ export const register = async (req, res) => {
       result
     })
   } catch (error) {
+    console.log(error)
     if (error.name === 'ValidationError') {
       res.status(StatusCodes.BAD_REQUEST).json({
         success: false,
